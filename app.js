f7d8d49c7f134a66823769d026955612c1936c65
Ilya200918 <95967188+Ilya200918@users.noreply.github.com>
2023-01-28 17:24:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function first() {
+    // Про 10+50 кнопка
+    alert(`Будет ${50+10}`)
+}
+
+second
+
+function second() {
+    //Спомощью Prompt
+    let age = prompt('Введите свой возрост')
+    if (age < 18) {
+        alert('Ты еще щчень маленький(ая)!')
+    } else {
+    alert(' Вход разрешен!')    
+    }
+}
+
+function third() {
+    // Про 10+50 кнопка
+ document.getElementById('zp').innerHTML = '1000$'
+}
+
